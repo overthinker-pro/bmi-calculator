ba9d19186ef745181108ca556f1b34049a8cf53c
overthinker-pro <noelvabraham@gmail.com>
2025-02-14 15:10:39 +0530

    BMI Calculator final

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log('Hello World');
+const btn = document.getElementById("calculate");
+
+btn.addEventListener("click", function () {
+  let height = document.querySelector("#height").value;
+  let weight = document.querySelector("#weight").value;
+
+  if (height === "" || weight == "") {
+    alert("Please fill out the input fields");
+
+    return;
+  }
+
+  // BMI = weight / height * height
+
+  height = height / 100;
+
+  let BMI = weight / (height * height);
+
+  BMI = BMI.toFixed(2);
+
+  document.querySelector("#result").innerHTML = BMI;
+
+  let status = "";
+
+  switch (true) {
+    case BMI < 18.5:
+      status = "Underweight";
+      break;
+    case BMI >= 18.5 && BMI < 25:
+      status = "Healthy";
+      break;
+    case BMI >= 25 && BMI < 30:
+      status = "Overweight";
+      break;
+    case BMI >= 30:
+      status = "Obese";
+      break;
+  }
+
+  document.querySelector(
+    ".comment"
+  ).innerHTML = `You are <span id="comment">${status}</span>`;
+});
