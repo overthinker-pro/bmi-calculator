bbf920bbfe8528cd6a4f7e83c52f534ae239f2eb
overthinker-pro <noelvabraham@gmail.com>
2025-02-14 15:13:02 +0530

    Calculator final change v1.2

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 const btn = document.getElementById("calculate");
 
-btn.addEventListener("click", function () {
-  let height = document.querySelector("#height").value;
-  let weight = document.querySelector("#weight").value;
+btn.addEventListener("click", function(){
+    let height = document.querySelector("#height").value;
+    let weight = document.querySelector("#weight").value;
 
-  if (height === "" || weight == "") {
-    alert("Please fill out the input fields");
+    if(height === "" || weight == ""){
+        alert("Please fill out the input fields");
 
-    return;
-  }
+        return;
+    }
 
-  // BMI = weight / height * height
 
-  height = height / 100;
+    // BMI = weight / height * height
 
-  let BMI = weight / (height * height);
 
-  BMI = BMI.toFixed(2);
+    height = height /100;
 
-  document.querySelector("#result").innerHTML = BMI;
 
-  let status = "";
+    let BMI = weight / (height * height);
 
-  switch (true) {
-    case BMI < 18.5:
-      status = "Underweight";
-      break;
-    case BMI >= 18.5 && BMI < 25:
-      status = "Healthy";
-      break;
-    case BMI >= 25 && BMI < 30:
-      status = "Overweight";
-      break;
-    case BMI >= 30:
-      status = "Obese";
-      break;
-  }
 
-  document.querySelector(
-    ".comment"
-  ).innerHTML = `You are <span id="comment">${status}</span>`;
+    BMI = BMI.toFixed(2);
+
+
+    document.querySelector("#result").innerHTML = BMI;
+
+
+    let status = "";
+
+    if (BMI < 18.5){
+        status = "Underweight";
+    }else if (BMI >= 18.5 && BMI < 25){
+        status = "Healthy";
+    }else if (BMI >= 25 && BMI < 30){
+        status = "Overweight";
+    }else if (BMI >= 30){
+        status = "Obese";
+    }
+
+
+    document.querySelector(".comment").innerHTML = `You are <span id="comment">${status}</span>`
 });
